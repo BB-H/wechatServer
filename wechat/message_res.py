4a401f7c534baec19a108006f08e95a7c6cae9ee
bb-h <ihlei001@gmail.com>
2016-03-18 16:55:17 +0000

    add template for returned message for wechat

---
+++ wechat/message_res.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+This file is used to define user visible message
+"""
+
+APP_NAME = "购买哩"
+
+USER_WELCOME = r'''感谢您关注%s!
+您可通过输入关键字查询您感兴趣的商品,比如输入:"想买 CK双肩包"''' %APP_NAME
+UNSUPPORT_MESSAGE = "success"
+REDUNDANT_MESSAGE = "Ignore redundant message"
+EMPTY_MESSAGE = "Empty incoming message"
